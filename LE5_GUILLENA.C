68847be1bced7ba66b66bc89f24aed6b6390de64
Akosifreddiejr <guillena.freddie07@gmail.com>
2025-09-25 21:36:05 +0800

    Add files via upload

---
+++ LE5_GUILLENA.C
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+
+    int num, sum = 0;
+
+    while (1) {
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+        if (num <= 0) {
+            break;
+        }
+
+        sum = sum+num;
+    }
+
+    printf("\nThe total sum of positive numbers is: %d", num);
+
+    return 0;
+}
+
+
+    
